53fd795d47e17afd7a30b5c5cfcaea20cbef5273
Diego Nuñez <diego.gabriel.nd@gmail.com>
2022-08-31 22:14:08 +0200

    Create base react project and structure

---
+++ frontend/src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div>
+        <h1>Calliper chart</h1>
+    </div>, document.getElementById("root")
+);
