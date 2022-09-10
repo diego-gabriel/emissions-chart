5ee5ed1bedf72775de713283b298a52de37093b9
Diego Nuñez <diego.gabriel.nd@gmail.com>
2022-09-10 23:01:58 +0200

    Add bem helpers. Will be useful for styling

---
+++ frontend/src/utils/bem.test.ts
@@ -0,0 +1,35 @@
+import bem from "./bem";
+
+describe('bem', () => {
+    const { bemBlock, bemElement } = bem('test-class');
+
+    describe('block builder', () => {
+        it('builds the correct class without modifiers', () => {
+            expect(bemBlock()).toEqual("test-class");
+        });
+
+        it.each`
+            modifiers | expectedClass
+            ${{ big: true, red: true}} | ${"test-class test-class--big test-class--red"}
+            ${{ big: true, red: false}} | ${"test-class test-class--big"}
+            ${{ big: false, red: false}} | ${"test-class"}
+        `('builds the correct class with modifiers', ({ modifiers, expectedClass}) => {
+            expect(bemBlock(modifiers)).toEqual(expectedClass);
+        });
+    });
+
+    describe('element builder', () => {
+        it('build the correct class without modifiers', () => {
+            expect(bemElement('element')).toEqual('test-class__element');
+        });
+
+        it.each`
+            modifiers | expectedClass
+            ${{ big: true, red: true}} | ${"test-class__element test-class__element--big test-class__element--red"}
+            ${{ big: true, red: false}} | ${"test-class__element test-class__element--big"}
+            ${{ big: false, red: false}} | ${"test-class__element"}
+        `('builds the correct class with modifiers', ({ modifiers, expectedClass}) => {
+            expect(bemElement('element', modifiers)).toEqual(expectedClass);
+        });
+    });
+})
---
+++ frontend/src/utils/bem.ts
@@ -0,0 +1,16 @@
+type BemModifiers = { [modifierName: string]: boolean };
+
+function buildWithModifiers(baseClass: string, modifiers: BemModifiers): string {
+    const buildModifiedClass = ([modifierName, active]: [string, boolean]) => active ? `${baseClass}--${modifierName}` : null;
+    const notNull = (value: string | null) => value !== null;
+
+    const withModifiers = Object.entries(modifiers).map(buildModifiedClass).filter(notNull);
+    return [baseClass, ...withModifiers].join(' ');
+}
+
+export default function bem(className: string) {
+    return {
+        bemBlock: (modifiers?: BemModifiers): string => buildWithModifiers(className, modifiers || {}),
+        bemElement: (element: string, modifiers?: BemModifiers): string => buildWithModifiers(`${className}__${element}`, modifiers || {}),
+    }
+}
