ceac49fdf8df0017b37c8823d17a5fa0d1cb54fd
Diego Nuñez <diego.gabriel.nd@gmail.com>
2022-09-12 01:53:40 +0200

    Delete unused code

--- frontend/src/reducers/RootReducer.ts
+++ frontend/src/reducers/RootReducer.ts
@@ -1,10 +1,8 @@
-import { currentUserReducer } from './CurrentUser';
 import { combineReducers } from 'redux';
 import { dataReducer } from './DataReducer';
 import { commentsReducer } from './CommentsReducer';
 
 const RootReducer = combineReducers({
-    currentUser: currentUserReducer,
     data: dataReducer,
     comments: commentsReducer,
 });
