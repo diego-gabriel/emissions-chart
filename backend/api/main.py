392cddb1a495ba5e4974ce6503d4ec1ed8c60a77
Diego Nuñez <diego.gabriel.nd@gmail.com>
2022-09-11 18:19:37 +0200

    Use data from the backend to draw chart

--- backend/api/main.py
+++ backend/api/main.py
@@ -3,8 +3,20 @@ import json
 import os
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/data")
--- frontend/src/epics/DataEpic.ts
+++ frontend/src/epics/DataEpic.ts
@@ -1,35 +1,17 @@
 import { Epic } from 'redux-observable';
-import { filter, map } from 'rxjs/operators';
-import { DataAction, dataActions, DataActionType } from '../reducers/DataReducer';
+import { filter, map, mergeMap } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+import { dataActions, DataActionType } from '../reducers/DataReducer';
 import { State } from '../store/Store';
-import Any = jasmine.Any;
 import { AnyAction } from 'redux';
-
-const dummyData = [
-    {
-        year: 1980,
-        emissions: 123,
-    },
-    {
-        year: 1990,
-        emissions: 234,
-    },
-    {
-        year: 2000,
-        emissions: 463,
-    },
-    {
-        year: 2010,
-        emissions: 793,
-    },
-    {
-        year: 2020,
-        emissions: 1092,
-    },
-];
+import { EmissionsData } from '../utils/types';
 
 export const dataEpic: Epic<AnyAction, AnyAction, State> = (actionStream$) =>
     actionStream$.pipe(
         filter((action) => action.type === DataActionType.GET_DATA),
-        map((action) => dataActions.getDataSuccess(dummyData))
+        mergeMap((action) =>
+            ajax
+                .get('http://127.0.0.1:8000/data')
+                .pipe(map((data) => dataActions.getDataSuccess(data.response as EmissionsData)))
+        )
     );
