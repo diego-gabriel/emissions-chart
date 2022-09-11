5815254cea6575db0b9903ee1f4642565d08c8bf
Diego Nuñez <diego.gabriel.nd@gmail.com>
2022-09-11 17:33:06 +0200

    Add /data enpoint and return mock data

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,3 +1,7 @@
+import csv
+import json
+import os
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,4 +9,12 @@ app = FastAPI()
 
 @app.get("/data")
 def read_data():
-    return [{"Hello": "World"}]
+    """ Data corresponds to 5-year average of  CO2 and greenhouse gas emissions per-capita
+    for the European Union (28) countries after 1900.
+    Source https://ourworldindata.org/co2/country/germany?country=~DEU"""
+
+    filename = os.path.join(os.path.dirname(__file__), "../../data/co-emissions-per-capita.json")
+
+    with open(filename) as data_file:
+        data = json.load(data_file)
+    return data
